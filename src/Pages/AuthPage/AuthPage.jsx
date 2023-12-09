87919c0c10a885cb7114a764ab7cecf3cf5e5896
Rakesh Kundu <kundur947@gmail.com>
2023-12-10 02:01:07 +0530

    Designed the auth page

--- src/Pages/AuthPage/AuthPage.jsx
+++ src/Pages/AuthPage/AuthPage.jsx
@@ -5,12 +5,19 @@ function AuthPage() {
   return (
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
       <Container maxW={"container.md"} padding={0}>
-        <Box display={{ base: "none", md: "block" }}>
-          <Image src="/auth.png" h={650} alt="phone image" />
-        </Box>
-        <VStack spacing={4} align={"stretch"}>
-          <AuthForm />
-        </VStack>
+        <Flex gap={10} alignItems={"center"} justifyContent={"center"}>
+          <Box display={{ base: "none", md: "block" }}>
+            <Image src="/auth.png" h={680} alt="phone image" />
+          </Box>
+          <VStack spacing={4} align={"stretch"}>
+            <AuthForm />
+            <Box textAlign={"center"}>Get the app.</Box>
+            <Flex gap={5} justifyContent={"center"}>
+              <Image src="/playstore.png" h={"10"} alt="app store" />
+              <Image src="/microsoft.png" h={"10"} alt="google play" />
+            </Flex>
+          </VStack>
+        </Flex>
       </Container>
     </Flex>
   );
